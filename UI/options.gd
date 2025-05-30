3aa35cf788316fd8ec5b1986e97ff9cf1705cf4d
simigdalius <150370765+simigdalius@users.noreply.github.com>
2025-05-30 16:16:55 +0300

    final

--- UI/options.gd
+++ UI/options.gd
@@ -1,11 +1,12 @@
 extends Control
 
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	add_window_mode_items()
 	opbutton.item_selected.connect(on_window_mode_selected)
-
+	$VBoxContainer/HSlider.value =db_to_linear(AudioServer.get_bus_volume_db(1))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -15,12 +16,7 @@ func _process(delta: float) -> void:
 func _on_backtomainmenu_pressed() -> void:
 	get_tree().change_scene_to_file("res://UIscenes/mainMenu.tscn")
 
-
-
-
-
 #gia optionsss
-
 @onready var opbutton = $VBoxContainer/OptionButton as OptionButton
 
 const WINDOW_MODE_ARRAY : Array[String] = [
@@ -40,7 +36,8 @@ func on_window_mode_selected(index:int)-> void:
 		1:
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 			DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, false)
+			
 
-
-func _on_h_slider_value_changed(value: float) -> void:
-	AudioServer.set_bus_volume_db(0,value)
+func _on_h_slider_mouse_exited() -> void:
+	release_focus()
+	AudioServer.set_bus_volume_db(0,linear_to_db($VBoxContainer/HSlider.value))
